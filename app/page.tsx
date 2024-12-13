477e0cbda25e081e37208c092162b56d0cf6bbc6
Akshat Bajetha <akshatbajetha@gmail.com>
2024-12-13 22:36:46 +0530

    Search

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import LoadingContainer from "@/components/global/LoadingContainer";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
+import { Suspense } from "react";
 
 function HomePage() {
   return (
     <>
       <Hero />
+      <Suspense fallback={<LoadingContainer />} />
       <FeaturedProducts />
     </>
   );
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,17 @@
-function ProductsPage() {
-  return <div>ProductsPage</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+type searchParams = {
+  layout?: string;
+  search?: string;
+};
+
+async function ProductsPage({ searchParams }: { searchParams: searchParams }) {
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+  return (
+    <>
+      <ProductsContainer layout={layout} search={search} />
+    </>
+  );
 }
 export default ProductsPage;
