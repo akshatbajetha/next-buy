97371e6e82bf1fb797b058338e207b16b9a054e2
Akshat Bajetha <akshatbajetha@gmail.com>
2024-12-14 21:18:34 +0530

    Fix deployment errors

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -37,7 +37,8 @@ function ProductsGrid({ products }: { products: Product[] }) {
               </Card>
             </Link>
             <div className="absolute top-7 right-7 z-5">
-              <FavoriteToggleButton productId={productId} />
+              {/* <FavoriteToggleButton productId={productId} /> */}
+              <FavoriteToggleButton />
             </div>
           </article>
         );
